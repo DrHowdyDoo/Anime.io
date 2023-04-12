e4cb74b9e0bf46a504a1bc227fed46d37ae192f3
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2023-04-12 14:56:19 +0530

    added material you color variant for app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.drhowdydoo.animenews"
         minSdk = 27
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
